cba000629a14814f84e59103d48251481c055a22
Chris <chtz@me.com>
2016-05-21 17:22:52 +0200

    proper cleanup

--- test.sh
+++ test.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 docker build -t foo/runruby .
-cat sample.txt | ruby exec.rb sample.rb | timelimit -t3 -s9 docker run -i --rm --net none foo/runruby
+CIDFILE=`uuid`.cid
+cat sample.txt | ruby exec.rb sample.rb | timelimit -t3 -s9 docker run -i --cidfile=$CIDFILE --net none foo/runruby
+CID=`head $CIDFILE`
+docker rm -f $CID
+rm $CIDFILE
