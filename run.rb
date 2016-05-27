6304a579f365c0d79c14d55826c256338a5d4b66
Chris <chtz@me.com>
2016-05-27 12:30:27 +0200

    env var support introduced

--- run.rb
+++ run.rb
@@ -1,9 +1,20 @@
 f = File.open("./script.rb", 'w')
+expect_script = true
 while line = gets
   if line =~ /~~~<666~END~OF~SCRIPT~999>~~~.*/
-    break
+    if expect_script
+      expect_script = false
+    else
+      break      
+    end
   else
-    f.write line
+    if expect_script
+      f.write line
+    else
+      if line =~ /(.+)=(.+)/
+        ENV[$1] = $2.chomp
+      end
+    end
   end
 end
 f.close
